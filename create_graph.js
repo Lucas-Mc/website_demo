f3cacf8a3d76977ffd123138b74736d1128deefe
Lucas-Mc <39505798+Lucas-Mc@users.noreply.github.com>
2020-07-17 08:10:26 -0400

    Reduces plot size again

--- create_graph.js
+++ create_graph.js
@@ -37,7 +37,7 @@ function createGraph(data) {
 
     data = [trace];
     layout = {
-        height: 600,
+        height: 400,
         title: 'Sample ECG Data',
         xaxis: {
             rangeslider: {}
