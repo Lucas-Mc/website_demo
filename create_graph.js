23479ebb7e394f7b293b4ad53611353b7b49bcbf
Lucas McCullum <lumccul3@gmail.com>
2020-07-18 15:44:14 -0400

    Refactors and improves documentation

--- create_graph.js
+++ create_graph.js
@@ -1,3 +1,8 @@
+// Simple demo to plot sample ECG data
+// Lucas McCullum
+// MIT-LCP
+// 2020
+
 // Select the desired input file
 input_files = ['demo_ecg1.csv', 'demo_ecg2.csv'];
 all_results = [];
@@ -5,7 +10,7 @@ data = [];
 shapes = [];
 
 function parseData(createGraph) {
-
+    // Read the input CSV files
     for (var k = 0; k < input_files.length; k++) {
         Papa.parse(input_files[k], {
             download: true,
@@ -21,13 +26,18 @@ function parseData(createGraph) {
 }
 
 function createGraph(all_results) {
-    // Line Plot.. start_at is the start of actual data in the CSV
+    // The starting line number of actual data in the CSV
     start_at = 0;
-    graphish = [];
+    temp_data = [];
+    // Set some initial conditions
+    grid_delta_minor = 0.04
+    grid_delta_major = 0.2
+    fs = [250, 250];
     sig_names = ['II', 'V'];
     units = ['mV', 'mV'];
     layout = {
-        height: 800,
+        // Dynamically determine the HTML figure size
+        height: 250 + all_results.length*250,
         title: 'Sample ECG Data',
         grid: {
             rows: input_files.length,
@@ -41,25 +51,18 @@ function createGraph(all_results) {
 
         index = [];
         signal = [];
-        temp_data = all_results[i].data;
-        if (i > 0 ) {
-            x_string = 'x' + (i+1).toString();
-            y_string = 'y' + (i+1).toString();
-            x_axis = 'xaxis' + (i+1).toString();
-            y_axis = 'yaxis' + (i+1).toString();
-        } else {
-            x_string = 'x';
-            y_string = 'y';
-            x_axis = 'xaxis'
-            y_axis = 'yaxis'
-        }
+        input_data = all_results[i].data;
+        // Name the axes to create the subplots
+        x_string = 'x' + (i+1).toString();
+        y_string = 'y' + (i+1).toString();
+        x_axis = 'xaxis' + (i+1).toString();
+        y_axis = 'yaxis' + (i+1).toString();
 
-        for (var j = start_at; j < temp_data.length; j++) {
-
-            fs = 250;
-            index.push(j / fs);
-            signal.push(temp_data[j][0]);
-
+        for (var j = start_at; j < input_data.length; j++) {
+            // Determine the time and value of the signal
+            index.push(j / fs[i]);
+            signal.push(input_data[j][0]);
+            // Create the signal to plot
             var trace = {
                 x: index,
                 y: signal,
@@ -75,13 +78,13 @@ function createGraph(all_results) {
                 name: sig_names[i]
             };
         }
-        graphish.push(trace);
-
-        grid_delta = 0.04
-        x_max = Math.ceil(Math.max(...graphish[i].x)/grid_delta)
-        y_min = Math.floor(Math.min(...graphish[i].y)/grid_delta)-1
-        y_max = Math.ceil(Math.max(...graphish[i].y)/grid_delta)+1
-
+        // Append the signal to temp data for analysis
+        temp_data.push(trace);
+        // Determine the bounds of the signal to draw the ECG grid
+        x_max = Math.ceil(Math.max(...temp_data[i].x)/grid_delta_minor)
+        y_min = Math.floor(Math.min(...temp_data[i].y)/grid_delta_minor)-1
+        y_max = Math.ceil(Math.max(...temp_data[i].y)/grid_delta_minor)+1
+        // Create the minor grids going horizontally (every 0.04 seconds)
         for (var j = y_min; j < y_max; j++) {
             shapes.push({
                 type:'rect',
@@ -89,54 +92,53 @@ function createGraph(all_results) {
                 xref: x_string,
                 yref: y_string,
                 x0: 0,
-                x1: x_max*grid_delta,
-                y0: 0+(j*grid_delta),
-                y1: grid_delta+(j*grid_delta),
+                x1: x_max*grid_delta_minor,
+                y0: 0+(j*grid_delta_minor),
+                y1: grid_delta_minor+(j*grid_delta_minor),
                 line: {
                     color: 'rgb(255, 212, 212)',
                     width: 1
                 }
             })
         }
-
+        // Create the minor grids going vertically (every grid_delta_minor seconds)
         for (var j = 0; j < x_max; j++) {
             shapes.push({
                 type:'rect',
                 layer: 'below',
                 xref: x_string,
                 yref: y_string,
-                x0: 0+(j*grid_delta),
-                x1: grid_delta+(j*grid_delta),
-                y0: y_min*grid_delta,
-                y1: y_max*grid_delta,
+                x0: 0+(j*grid_delta_minor),
+                x1: grid_delta_minor+(j*grid_delta_minor),
+                y0: y_min*grid_delta_minor,
+                y1: y_max*grid_delta_minor,
                 line: {
                     color: 'rgb(255, 212, 212)',
                     width: 1
                 }
             })
         }
-
+        // Determine the major grid lines (every grid_delta_major seconds)
         layout[x_axis] = {
             title: 'Time (s)',
-            dtick: 0.2,
+            dtick: grid_delta_major,
             gridcolor: 'rgb(255, 60, 60)',
             gridwidth: 1
         }
         layout[y_axis] = {
             title: sig_names[i] + ' (' + units[i] + ')',
             fixedrange: true,
-            dtick: 0.2,
+            dtick: grid_delta_major,
             gridcolor: 'rgb(255, 60, 60)',
             gridwidth: 1
         }
-
-        data.push(graphish);
-
+        // Append the temp data to the final data
+        data.push(temp_data);
     }
-
+    // Add the gridlines
     layout['shapes'] = shapes;
+    // Plot the final signal with the desired layout
     Plotly.newPlot("chart", data[0], layout); 
-
 }
-
+// Call the data-parser so it can create the graph
 parseData(createGraph)
